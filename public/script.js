ac47fadbfb5edae174294b6f4bb5bd9fd7ad5c47
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-04-25 00:59:54 +0530

    deployed mapbox url added , map code refactor

--- public/script.js
+++ public/script.js
@@ -1,32 +1,74 @@
 mapboxgl.accessToken =
   "pk.eyJ1IjoibXJpZ2VzaHRoYWt1ciIsImEiOiJjbDdwdjZ2MG4wbGVmM3JzMzVtb2U1MnJ0In0.nbEGuAgv1N1c-tXDyR7d4g";
 
-navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
-  enableHighAccuracy: true,
-});
-
 const queryString = window.location.pathname.split("p");
 console.log(queryString[1], "here");
-const url = `http://localhost:3000/himraahi/map${queryString[1]}`;
+const url = `https://rodhak11.onrender.com/himraahi/map${queryString[1]}`;
 console.log(url);
-function successLocation(position) {
-  setupMap([position.coords.longitude, position.coords.latitude]);
-}
-const response = await fetch(url, { method: "GET" });
-const data = await response.json();
-console.log(data.data.currentCoordinates);
 
-function errorLocation() {
-  setupMap([-2.24, 53.48]);
+try {
+  setInterval(() => {
+    fetch(url, { method: "GET" })
+      .then(async (response) => {
+        const data1 = await response.json();
+        console.log(response, data1);
+        setupMap(
+          data1.data.currentCoordinates,
+          data1.data.Start,
+          data1.data.End
+        );
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, 10000);
+} catch (error) {
+  console.log("There was an error", error);
 }
 
-function setupMap(center) {
+function setupMap(center, start, end) {
+  console.log(center);
   const map = new mapboxgl.Map({
     container: "map",
     style: "mapbox://styles/mapbox/navigation-night-v1",
     center: center,
     zoom: 15,
   });
+  console.log(map);
+
+  const geojson = {
+    type: "FeatureCollection",
+    features: [
+      {
+        type: "Feature",
+        geometry: {
+          type: "Point",
+          coordinates: center,
+        },
+        properties: {
+          title: "Rodhak",
+          description: "Driver's Current Location",
+        },
+      },
+    ],
+  };
+  // add markers to map
+  for (const feature of geojson.features) {
+    // create a HTML element for each feature
+    const el = document.createElement("div");
+    el.className = "marker";
+
+    // make a marker for each feature and add to the map
+    new mapboxgl.Marker(el)
+      .setLngLat(feature.geometry.coordinates)
+      .setPopup(
+        new mapboxgl.Popup({ offset: 25 }) // add popups
+          .setHTML(
+            `<h3>${feature.properties.title}</h3><p>${feature.properties.description}</p>`
+          )
+      )
+      .addTo(map);
+  }
 
   const route = new MapboxDirections({
     accessToken: mapboxgl.accessToken,
@@ -39,115 +81,8 @@ function setupMap(center) {
     congestion: true,
   });
 
+  route.setOrigin(start);
+  route.setDestination(end);
+
   map.addControl(route, "top-left");
-
-  map.on("load", async () => {
-    map.addSource("iss", {
-      type: "geojson",
-      data: center,
-    });
-
-    //add image
-
-    const geojson = {
-      type: "FeatureCollection",
-      features: [
-        {
-          type: "Feature",
-          geometry: {
-            type: "Point",
-            coordinates: data.data.currentCoordinates,
-          },
-          properties: {
-            title: "Rodhak",
-            description: "Driver's Current Location",
-          },
-        },
-      ],
-    };
-    // add markers to map
-    for (const feature of geojson.features) {
-      // create a HTML element for each feature
-      const el = document.createElement("div");
-      el.className = "marker";
-
-      // make a marker for each feature and add to the map
-      new mapboxgl.Marker(el)
-        .setLngLat(feature.geometry.coordinates)
-        .setPopup(
-          new mapboxgl.Popup({ offset: 25 }) // add popups
-            .setHTML(
-              `<h3>${feature.properties.title}</h3><p>${feature.properties.description}</p>`
-            )
-        )
-        .addTo(map);
-    }
-
-    //add directions
-
-    const directions = new MapboxDirections({
-      accessToken: mapboxgl.accessToken,
-      unit: "metric",
-      controls: { instructions: false, interactive: false },
-    });
-
-    map.addControl(directions, "top-left");
-
-    directions.setOrigin(data.data.Start);
-    directions.setDestination(data.data.End);
-    const nav = new mapboxgl.NavigationControl();
-
-    map.addControl(nav);
-
-    map.addLayer({
-      id: "iss",
-      type: "symbol",
-      source: "iss",
-      layout: {
-        "icon-image": "{marker-symbol}-15",
-        "text-field": "{title}",
-        "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-        "text-offset": [0, 0.6],
-        "text-anchor": "top",
-      },
-    });
-
-    // Update the source from the API every 2 seconds.
-    const updateSource = setInterval(async () => {
-      const geojson = await getLocation(updateSource);
-      map.getSource("iss").setData(center);
-    }, 2000);
-
-    async function getLocation(updateSource) {
-      // Make a GET request to the API and return the location of the Driver
-      try {
-        const response = await fetch(url, { method: "GET" });
-        const data = await response.json();
-        console.log(data, data.data.currentCoordinates);
-
-        // Fly the map to the location.
-        map.flyTo({
-          center: data.data.currentCoordinates,
-          speed: 0.5,
-        });
-        // Return the location of the driver as GeoJSON.
-        return {
-          type: "FeatureCollection",
-          features: [
-            {
-              type: "Feature",
-              geometry: {
-                type: "Point",
-                coordinates: data.data.currentCoordinates,
-              },
-            },
-          ],
-        };
-      } catch (err) {
-        // If the updateSource interval is defined, clear the interval to stop updating the source.
-        if (updateSource) clearInterval(updateSource);
-        throw new Error(err);
-      }
-    }
-  });
 }
