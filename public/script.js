7486fbb97b5a021a2d00d8277c5098544ebc9b91
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-06-15 21:09:44 +0530

    changes in user map

--- public/script.js
+++ public/script.js
@@ -7,22 +7,16 @@ const url = `https://rodhak11.onrender.com/himraahi/map${queryString[1]}`;
 console.log(url);
 
 try {
-  setInterval(() => {
-    fetch(url, { method: "GET" })
-      .then(async (response) => {
-        const data1 = await response.json();
-        console.log(response, data1);
+  fetch(url, { method: "GET" })
+    .then(async (response) => {
+      const data1 = await response.json();
+      console.log(response, data1);
 
-        setupMap(
-          data1.data.currentCoordinates,
-          data1.data.Start,
-          data1.data.End
-        );
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, 2000);
+      setupMap(data1.data.currentCoordinates, data1.data.Start, data1.data.End);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 } catch (error) {
   console.log("There was an error", error);
 }
@@ -35,55 +29,74 @@ function setupMap(center, start, end) {
     center: center,
     zoom: 15,
   });
-  console.log(map);
-
-  const geojson = {
-    type: "FeatureCollection",
-    features: [
-      {
-        type: "Feature",
-        geometry: {
-          type: "Point",
-          coordinates: center,
-        },
-        properties: {
-          title: "Rodhak",
-          description: "Driver's Current Location",
-        },
-      },
-    ],
-  };
-  // add markers to map
-  for (const feature of geojson.features) {
-    // create a HTML element for each feature
-    const el = document.createElement("div");
-    el.className = "marker";
-
-    // make a marker for each feature and add to the map
-    new mapboxgl.Marker(el)
-      .setLngLat(feature.geometry.coordinates)
-      .setPopup(
-        new mapboxgl.Popup({ offset: 25 }) // add popups
-          .setHTML(
-            `<h3>${feature.properties.title}</h3><p>${feature.properties.description}</p>`
-          )
-      )
-      .addTo(map);
-  }
+  // console.log(map);
 
   const route = new MapboxDirections({
     accessToken: mapboxgl.accessToken,
     unit: "metric",
     controls: {
-      instructions: false,
       inputs: false,
+      instructions: false,
     },
+
     interactive: false,
     congestion: true,
   });
 
+  console.log(route.actions.setOptions);
+  // route.controls.instructions = false;
   route.setOrigin(start);
   route.setDestination(end);
 
   map.addControl(route, "top-left");
+
+  // updating the bus location
+  try {
+    setInterval(() => {
+      fetch(url, { method: "GET" })
+        .then(async (response) => {
+          const data1 = await response.json();
+          console.log(response, data1);
+
+          const geojson = {
+            type: "FeatureCollection",
+            features: [
+              {
+                type: "Feature",
+                geometry: {
+                  type: "Point",
+                  coordinates: data1.data.currentCoordinates,
+                },
+                properties: {
+                  title: "Rodhak",
+                  description: "Driver's Current Location",
+                },
+              },
+            ],
+          };
+          // add markers to map
+          for (const feature of geojson.features) {
+            // create a HTML element for each feature
+            const el = document.createElement("div");
+            el.className = "marker";
+
+            // make a marker for each feature and add to the map
+            new mapboxgl.Marker(el)
+              .setLngLat(feature.geometry.coordinates)
+              .setPopup(
+                new mapboxgl.Popup({ offset: 25 }) // add popups
+                  .setHTML(
+                    `<h3>${feature.properties.title}</h3><p>${feature.properties.description}</p>`
+                  )
+              )
+              .addTo(map);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }, 2000);
+  } catch (error) {
+    console.log("There was an error", error);
+  }
 }
